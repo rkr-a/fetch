fad4fcab1800cd8dff2f9a9eaa8eb801696b5393
r.a <2023-1-00960@lpu.edu.ph>
2025-06-14 11:02:01 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBwLa0dgC78I_sGqd5V6NDxsjqtMO3yXhA",
+  authDomain: "sign-2a2fd.firebaseapp.com",
+  databaseURL: "https://sign-2a2fd-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sign-2a2fd",
+  storageBucket: "sign-2a2fd.firebasestorage.app",
+  messagingSenderId: "529880627916",
+  appId: "1:529880627916:web:ce4836964d3228fe2a2069"
+};
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
